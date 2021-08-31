bb65e153ad744d9d598e17c52a2f4508facefe00
jackwaghorn <47627707+jackwaghorn@users.noreply.github.com>
2021-08-31 19:05:53 +0100

    again

---
+++ netlify/functions/prismic.js
@@ -0,0 +1,30 @@
+const Prismic = require("@prismicio/client");
+
+exports.handler = async function () {
+  const client = Prismic.client("http://roxanakenjeeva.cdn.prismic.io/api");
+  return client
+    .query(Prismic.Predicates.at("document.type", "product")) // An empty query will return all the documents
+    .then(function (response) {
+      console.log("Documents: ", response.results);
+
+      let data = response.results;
+
+      let newItems = Object.values(data).map((item) => {
+        return {
+            name: item.data.product.name.value[0].text,
+            id: item.uid,
+            price: item.data.product.price.value,
+            url: 'https://serverless-rox.netlify.app/.netlify/functions/prismic',
+        };
+      });
+
+      return {
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(newItems),
+      };
+    });
+};
