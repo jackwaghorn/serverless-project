0b3f49d34d842e2397f4c85186bca549e02ba0a0
jackwaghorn <47627707+jackwaghorn@users.noreply.github.com>
2021-09-01 11:18:39 +0100

    again

---
+++ netlify/functions/snipcart-inventory.js
@@ -0,0 +1,35 @@
+const fetch = require("node-fetch");
+
+exports.handler = async function () {
+  const secret =
+    "ST_YWM2YmI1NWMtOGJmYS00ZmMzLThjMmUtYzQ2ZGNiYmU1ZGRiNjM3NjYwODY0NDgzODQ2ODg4";
+
+  const request = await fetch("https://app.snipcart.com/api/products", {
+    headers: {
+      Authorization: `Basic ${Buffer.from(secret).toString("base64")}`,
+      Accept: "application/json",
+    },
+  });
+
+  const result = await request.json();
+
+  
+
+  let newItems = Object.values(result.items).map((item) => {
+    return {
+        name: item.name,
+        id: item.userDefinedId,
+        stock: item.stock
+    };
+  });
+
+
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(newItems),
+  };
+};
